87d23d2c73200b4a8810deec2427456a91935691
chihiro1010 <chihiroohba.info@gmail.com>
2023-11-24 22:11:07 +0900

    途中確認

--- js/index.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  document.body.classList.add("animation-in-progress");
-
-  setTimeout(function () {
-    document.body.classList.remove("animation-in-progress");
-  }, 10000); // アニメーションの時間（10秒）に合わせて調整
-});
